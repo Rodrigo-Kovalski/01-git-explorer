c7d3c4b9b718aa07f6a85fe9af479d3c8150c269
Rodrigo Kovalski <56686443+Rodrigo-Kovalski@users.noreply.github.com>
2021-09-21 20:41:05 -0300

    Add files via upload

---
+++ src/components/RepositoryList.tsx
@@ -0,0 +1,36 @@
+import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+import { useState , useEffect } from "react";
+
+/*const repository = {
+    name: 'unform',
+    description: 'forms in react',
+    link: 'https://github.com/'
+} Variável estática que usamos para entender as propriedades de um componente */
+
+interface Repository {
+    name: string,
+    description: string,
+    html_url: string
+}
+
+export function RepositoryList () {
+    const [repositories, setRepositories] = useState<Repository[]>([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data));
+    },[]);
+
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            <ul>
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository}/>
+                })}
+            </ul>
+        </section>
+    );
+}
